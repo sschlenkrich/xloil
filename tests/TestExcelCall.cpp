be84751c0a6c6031fa3687d5f73f0400a91bad52
Steven <cunnane@gmail.com>
2026-07-09 19:19:19 +0200

    Fix excel call name-to-number mapping which seems to have gone wrong when xlCoerce was added.  Add much better test.

--- tests/TestExcelCall.cpp
+++ tests/TestExcelCall.cpp
@@ -1,5 +1,7 @@
 #include "CppUnitTest.h"
 #include <xlOil/ExcelCall.h>
+#include <xlOil-XLL/ExcelCallMapping.h>
+
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -17,10 +19,20 @@ namespace Tests
     {
       Assert::AreEqual(excelFuncNumber("onWindow"), msxll::xlcOnWindow);
       Assert::AreEqual(excelFuncNumber("foobar"), -1);
-      Assert::AreEqual(excelFuncNumber(excelFuncName(msxll::xlcOnWindow)), msxll::xlcOnWindow);
       Assert::AreEqual(excelFuncName(999), nullptr);
       Assert::AreEqual(excelFuncNumber("n"), msxll::xlfN);
-      Assert::AreEqual(excelFuncNumber(excelFuncName(msxll::xlfT)), msxll::xlfT);
+
+      for (auto entry: FuncMap::sortedNames) 
+      {
+        auto num = excelFuncNumber(entry.name);
+        Assert::AreEqual((int)entry.number, num, utf8ToUtf16(entry.name).c_str());
+        if ((num & msxll::xlSpecial) != 0)
+          continue; // We don't cover the reverse map for the specials
+        auto name = excelFuncName(entry.number);
+        if (!name && ((num & msxll::xlCommand) != 0))
+          continue; // We don't cover some of the higher numbered commands
+        Assert::AreEqual(entry.name, excelFuncName(entry.number));
+      }
     }
   };
 }
